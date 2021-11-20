2b3fba157d190bb96ba5f68dcaeb43a0ec5f5f1b
Chandrika C <chandrikakasturi33@gmail.com>
2021-11-20 18:35:00 +0530

    npm package install component generate
    -update node_modules

--- B16BBookKart/src/app/app.module.ts
+++ B16BBookKart/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MyordersComponent } from './myorders/myorders.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyordersComponent
   ],
   imports: [
     BrowserModule,
